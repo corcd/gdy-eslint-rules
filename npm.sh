1af37407aedb74efcd5ce2d7303e2216e0293091
王含泽 <whzcorcd@gmail.com>
2019-10-31 14:55:33 +0800

    #1 规则更新

---
+++ npm.sh
@@ -0,0 +1,4 @@
+node -v
+sudo npm upgrade
+sudo npm install eslint -save--dev  
+sudo npm install eslint-plugin-vue@latest eslint-config-google@latest -save--dev 
---
+++ yarn.sh
@@ -0,0 +1,4 @@
+node -v
+sudo yarn upgrade
+sudo yarn add eslint -s --dev  
+sudo yarn add eslint-plugin-vue@latest eslint-config-google@latest -s --dev
