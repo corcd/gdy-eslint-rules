a4b88bdce5028ba2e93c69410cc8e994cedf6bd0
王含泽 <whzcorcd@gmail.com>
2019-11-05 13:44:11 +0800

    #1 规则更新

--- shell/npm.sh
+++ shell/npm.sh
@@ -1,4 +1,4 @@
 node -v
 sudo npm install eslint -save--dev
-sudo npm install prettier eslint-plugin-prettier eslint-config-prettier prettier-eslint prettier-eslint-cli -save--dev
-sudo npm install babel-eslint eslint-friendly-formatter eslint-loader eslint-plugin-html eslint-config-standard eslint-plugin-promise eslint-plugin-standard eslint-plugin-import eslint-plugin-node eslint-plugin-flow-vars eslint-plugin-react eslint-config-vue eslint-plugin-vue eslint-config-google -save--dev 
+sudo npm install prettier eslint-plugin-prettier eslint-config-prettier prettier-eslint prettier-eslint-cli --save--dev
+sudo npm install babel-eslint eslint-friendly-formatter eslint-loader eslint-plugin-html eslint-config-standard eslint-plugin-promise eslint-plugin-standard eslint-plugin-import eslint-plugin-node eslint-plugin-flow-vars eslint-plugin-react eslint-config-vue eslint-plugin-vue eslint-config-google --save--dev 
